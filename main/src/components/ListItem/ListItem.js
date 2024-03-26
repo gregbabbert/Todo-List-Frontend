ea87bd3a699ea639c827419f73ba2ae9e5ecc8f7
Greg Babbert <gdbabbert@gmail.com>
2024-03-25 23:46:42 -0400

    updated styling

--- main/src/components/ListItem/ListItem.js
+++ main/src/components/ListItem/ListItem.js
@@ -3,7 +3,7 @@ import './ListItem.css';
 
 function ListItem({task}) {
     return (
-        <div style={{backgroundColor: 'red', width: '600px', marginLeft: 'auto', marginRight: 'auto', padding: '5px 0px', marginBottom: '20px'}} >
+        <div style={{backgroundColor: 'blue', width: '600px', marginLeft: 'auto', marginRight: 'auto', padding: '5px 0px', marginBottom: '20px'}} >
             <h3>{task.title}</h3>
             <p>{task.description}</p>
         </div>
