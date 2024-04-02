02586d229dcdc6da212d410f4b6b9d831c827590
Greg Babbert <gdbabbert@gmail.com>
2024-04-01 23:40:29 -0400

    added buttons for edit and delete

--- main/src/components/ListItem/ListItem.js
+++ main/src/components/ListItem/ListItem.js
@@ -6,6 +6,10 @@ function ListItem({task}) {
         <div style={{backgroundColor: 'blue', width: '600px', marginLeft: 'auto', marginRight: 'auto', padding: '5px 0px', marginBottom: '20px'}} >
             <h3>{task.title}</h3>
             <p>{task.description}</p>
+            <div>
+                <button type="button">Edit</button>
+                <button type="button">Delete</button>
+            </div>
         </div>
     );
 }
