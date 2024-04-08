62195bd6de0c9d3750777ded90842d62ef33d516
Greg Babbert <gdbabbert@gmail.com>
2024-04-07 22:53:12 -0400

    created component for editing items

---
+++ main/src/components/EditItem/EditItem.js
@@ -0,0 +1,27 @@
+import React, { Component, useState } from 'react';
+import './EditItem.css';
+
+let taskData
+
+function EditItem({task, index, deleteTask}) {
+
+    const [update, setUpdate] = useState(false);
+
+    if (!update) {
+        return (
+            <div style={{backgroundColor: 'blue', width: '600px', marginLeft: 'auto', marginRight: 'auto', padding: '5px 0px', marginBottom: '20px'}} >
+                <h3>{task.title}</h3>
+                <p>{task.description}</p>
+                <div>
+                    <button type="button">Edit</button>
+                    <button type="button" onClick={() => {
+                        deleteTask(index);
+                        setUpdate(true);
+                        }} >Delete</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default EditItem;
