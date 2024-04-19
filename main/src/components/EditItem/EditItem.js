5cf26ece8f2396b2a5ee2d7c52cfbb05dba65877
Greg Babbert <gdbabbert@gmail.com>
2024-04-18 23:44:27 -0400

    updated color

--- main/src/components/EditItem/EditItem.js
+++ main/src/components/EditItem/EditItem.js
@@ -9,7 +9,7 @@ function EditItem({task, index, deleteTask}) {
 
     if (!update) {
         return (
-            <div style={{backgroundColor: 'blue', width: '600px', marginLeft: 'auto', marginRight: 'auto', padding: '5px 0px', marginBottom: '20px'}} >
+            <div style={{backgroundColor: 'green', width: '600px', marginLeft: 'auto', marginRight: 'auto', padding: '5px 0px', marginBottom: '20px'}} >
                 <h3>{task.title}</h3>
                 <p>{task.description}</p>
                 <div>
